985775ee4e65e0d3bf4cd6abb56d00cd1377d474
Randikaviraj <randikavirajmax@gmail.com>
2021-04-01 05:39:38 +0530

    dashboardUpdate func added

--- Server/db/dashboardDb.py
+++ Server/db/dashboardDb.py
@@ -1,8 +1,13 @@
 from Server.db import  db
+import threading
+from pymongo import ReturnDocument
 
 
 # get reference to Dahboard collection
 dashboard=db['Dashboard']
+
+# global lock for threads
+lock=threading.Lock()
 
 class Dashboard:
     def __init__(self,email,name) -> None:
@@ -27,3 +32,31 @@ def deleteDashBoard(email,name):
         dashboard.delete_one({"email":email,"name":name}) 
     except:
       print('An exception occurred deleteDashBoard')
+      
+      
+def updateDashboard(day: str,hour: int,email,name,value: str,period: int): 
+    # value as Not Available or availble
+    try:
+        dash=getDashBoard(email,name)
+        with lock:
+            for i in range(period):
+                if len(dash[day][str(hour+period)])>0:
+                    dash[day][str(hour+period)]=value
+                else:
+                    return None
+            new=dash[day]
+            return dashboard.find_one_and_update({'email':email,'name':name},
+                        { '$set': { day : new} }, 
+                        return_document = ReturnDocument.AFTER)               
+    except:
+        print('An exception occurred updateDashboard')
+        raise Exception("Exption")
+      
+      
+def getDashBoard(email,name):
+    try:
+      dash=dashboard.find_one({'email':email,'name':name})
+      return dash
+    except:
+      print('An exception occurred getDashBoard')
+      raise Exception("Dashboard Error")
